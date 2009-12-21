202991f144cafde41d825811b0a1ec15f35776d2
Decklin Foster <decklin@red-bean.com>
2009-12-21 10:54:13 -0500

    Put in a warning

--- center-image.js
+++ center-image.js
@@ -2,6 +2,9 @@ var html = document.documentElement;
 var body = document.body;
 var imgs = document.getElementsByTagName('img');
 
+// This is a rather hacky heuristic for determining if we are in fact on a
+// generated image "page". It might break at any time.
+
 if (html.childNodes.length == 1 && html.firstChild == body &&
         body.childNodes.length == 1 && body.firstChild == imgs[0]) {
     with (imgs[0].style) {
