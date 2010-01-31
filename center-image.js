10f428d284b046b9947eed25316dcc70dd30f640
Decklin Foster <decklin@red-bean.com>
2010-01-31 09:02:11 -0500

    Much cleaner image-page heuristic

--- center-image.js
+++ center-image.js
@@ -1,22 +1,16 @@
-var html = document.documentElement;
-var body = document.body;
-var imgs = document.getElementsByTagName('img');
-
 // This is a rather hacky heuristic for determining if we are in fact on a
 // generated image "page". It might break at any time.
 
-if (html.childNodes.length == 1 && html.firstChild == body &&
-    body.childNodes.length == 1 && body.firstChild == imgs[0])
-{
-    html.style.display = 'table';
-    html.style.width = '100%';
-    html.style.height = '100%';
+if (document.images && document.images[0].src == location.href) {
+    document.documentElement.style.display = 'table';
+    document.documentElement.style.width = '100%';
+    document.documentElement.style.height = '100%';
 
-    body.style.display = 'table-cell';
-    body.style.verticalAlign = 'middle';
-    body.style.textAlign = 'center';
+    document.body.style.display = 'table-cell';
+    document.body.style.verticalAlign = 'middle';
+    document.body.style.textAlign = 'center';
 
     chrome.extension.sendRequest('bgcolor', function(response) {
-        body.style.backgroundColor = response;
+        document.body.style.backgroundColor = response;
     });
 }
